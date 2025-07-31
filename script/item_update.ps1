5cd061bd56e1f700f6f43efa6da935f812fc6cf2
CZM <chengzm23@mails.tsinghua.edu.cn>
2025-07-31 09:30:49 +0800

    用utf-8 with bom保存脚本

--- script/item_update.ps1
+++ script/item_update.ps1
@@ -1,19 +1,19 @@
-# This script updates the item data for the Missile Wars.
+ï»¿# This script updates the item data for the Missile Wars.
 
 $script_dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Set-Location $script_dir/»ÊÊÒµ¼µ¯Õ½ÕùÊı¾İ°ü¸üĞÂ
+Set-Location $script_dir/çš‡å®¤å¯¼å¼¹æˆ˜äº‰æ•°æ®åŒ…æ›´æ–°
 py.exe .\description_updator.py
 py.exe .\cost_updator.py
 py.exe .\book_updator.py
 
-py.exe .\compare_packs.py .\data "D:\czmÎÒµÄÊÀ½ç\.minecraft\versions\1.21-Fabric 0.15.11\saves\Missile Wars\datapacks\Missile Wars Fusion for 1.21\data" --output .\diff.txt
+py.exe .\compare_packs.py .\data "D:\czmæˆ‘çš„ä¸–ç•Œ\.minecraft\versions\1.21-Fabric 0.15.11\saves\Missile Wars\datapacks\Missile Wars Fusion for 1.21\data" --output .\diff.txt
 
 $choice = Read-Host "Do you want to apply the changes? (y/n)"
 
 $choice = $choice.ToLower()
 
 if ($choice -eq 'y') {
-    py.exe .\sync_packs.py .\data "D:\czmÎÒµÄÊÀ½ç\.minecraft\versions\1.21-Fabric 0.15.11\saves\Missile Wars\datapacks\Missile Wars Fusion for 1.21\data"
+    py.exe .\sync_packs.py .\data "D:\czmæˆ‘çš„ä¸–ç•Œ\.minecraft\versions\1.21-Fabric 0.15.11\saves\Missile Wars\datapacks\Missile Wars Fusion for 1.21\data"
     Write-Host "Changes applied successfully."
 } else {
     Write-Host "No changes were applied."
